f890bc7b009ef43067942980b4b6d6905da76416
dantleech <dan.t.leech@gmail.com>
2018-02-18 12:44:24 +0100

    Callback sampler

---
+++ lib/Sampler/CallbackSampler.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PMeter\Sampler;
+
+use Closure;
+use Generator;
+
+class CallbackSampler
+{
+    /**
+     * @var Closure
+     */
+    private $closure;
+
+    /**
+     * @var int
+     */
+    private $revs;
+
+    public function __construct(Closure $closure, int $revs)
+    {
+        $this->closure = $closure;
+        $this->revs = $revs;
+    }
+
+    public function __invoke(): Generator
+    {
+        $data = yield;
+
+        while (true) {
+            $closure = $this->closure;
+            $start = microtime(true);
+
+            for ($i = 0; $i < $this->revs; $i++) {
+                $closure($data);
+            }
+
+            $time = microtime(true) - $start;
+
+            $data = yield array_merge($data, [
+                'time' => $time
+            ]);
+        }
+    }
+}
